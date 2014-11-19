672a36c0689844f0220123c1a67eb5db485594c5
toannguyenle <toan@me.com>
2014-11-18 16:20:34 -0800

    SPA with AngularJS and API endpoints set up

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ Feedmeapp::Application.routes.draw do
   # Root Routing
   root 'application#index'
   
+  # Rerouting everything else to index page
+  get '*path' => 'application#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
