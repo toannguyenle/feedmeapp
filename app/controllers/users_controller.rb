87dbd68c26c3916c34b4e1095ac81d576dd83279
nataliaSumali <natalia.sumali@gmail.com>
2014-11-19 19:12:19 -0800

    created users_controller.rb

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+  def index
+    users = User.all
+    render json: users, status: 200
+  end
+
+  def new
+    user = User.new
+    # render json: user, status: 201
+  end
+  
+  def create
+    user = User.create(user_params)
+    render json: user, status: 201
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update_attributes(user_params)
+    render nothing: true, status: 204
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    render nothing: true, status: 204
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :phone_number, :lat, :lng, :order_id)
+  end
+end
