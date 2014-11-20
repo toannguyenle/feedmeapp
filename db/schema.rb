b1a5c38866ed49189581dbd9e781dbb11bcb2600
nataliaSumali <natalia.sumali@gmail.com>
2014-11-19 18:19:41 -0800

    modified schema

--- db/schema.rb
+++ db/schema.rb
@@ -22,14 +22,20 @@ ActiveRecord::Schema.define(version: 20141118031733) do
     t.string   "estimated_delivery_time"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "order_id"
   end
+
+  add_index "deliveries", ["order_id"], name: "index_deliveries_on_order_id", using: :btree
 
   create_table "details", force: true do |t|
     t.string   "status"
     t.string   "additional_info"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "order_id"
   end
+
+  add_index "details", ["order_id"], name: "index_details_on_order_id", using: :btree
 
   create_table "orders", force: true do |t|
     t.string   "product_count"
@@ -51,7 +57,10 @@ ActiveRecord::Schema.define(version: 20141118031733) do
     t.string   "status"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "order_id"
   end
+
+  add_index "payments", ["order_id"], name: "index_payments_on_order_id", using: :btree
 
   create_table "products", force: true do |t|
     t.string   "name"
@@ -95,6 +104,9 @@ ActiveRecord::Schema.define(version: 20141118031733) do
     t.string   "lng"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "order_id"
   end
+
+  add_index "users", ["order_id"], name: "index_users_on_order_id", using: :btree
 
 end
