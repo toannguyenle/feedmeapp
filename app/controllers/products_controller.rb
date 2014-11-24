1c5181618a8b4c7d6adcb066d194d7d461dfe410
toannguyenle <toan@me.com>
2014-11-24 15:02:38 -0800

    nothing change - just accidentally open some file on master

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,12 +4,14 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all
+    
     # ORDRIN TEST
     require "ordrin"
     ordrin_api = Ordrin::APIs.new(ENV["OD_SECRET"], :test)
-    raise ordrin_params.inspect
+    raise params[:ordrin].inspect
     args = {:datetime => 'ASAP', :zip => '90401', :city => 'Santa Monica',:addr => '1520 2nd St'}
     delivery_list = ordrin_api.delivery_list(args)
+
     render json: delivery_list, status: 200
   end
 
@@ -17,7 +19,7 @@ class ProductsController < ApplicationController
     # ORDRIN TEST
     require "ordrin"
     ordrin_api = Ordrin::APIs.new(ENV["OD_SECRET"], :test)
-    raise ordrin_params.inspect
+    raise params[:ordrin].inspect
     args = {:datetime => 'ASAP', :zip => '90401', :city => 'Santa Monica',:addr => '1520 2nd St'}
     delivery_list = ordrin_api.delivery_list(args)
     render json: delivery_list, status: 200
@@ -72,10 +74,10 @@ class ProductsController < ApplicationController
     def set_product
       @product = Product.find(params[:id])
     end
-    def ordrin_params
-      params.require(:ordrin).permit(:ordrin_zip, :ordrin_route, :ordrin_city, :ordrin_budget)
-    end
+    # def ordrin_params
+    #   params.require(:ordrin).permit(:ordrin_zip, :ordrin_route, :ordrin_city, :ordrin_budget)
+    # end
     def product_params
-      params.require(:product).permit(:name, :categories, :description, :image_urls, :regular_price, :discount_price, :discount_start_time, :discount_end_time, :discount_inventory, :ordr, :delivery_method, :restaurant_id)
+      params.require(:product).permit(:ordrin, :name, :categories, :description, :image_urls, :regular_price, :discount_price, :discount_start_time, :discount_end_time, :discount_inventory, :ordr, :delivery_method, :restaurant_id)
     end
 end
