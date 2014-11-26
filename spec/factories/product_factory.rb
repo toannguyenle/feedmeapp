60db60ce37511cc56d6ae716c1adaf8434143780
nataliaSumali <natalia.sumali@gmail.com>
2014-11-26 09:26:29 -0800

    testing on order, not done yet

---
+++ spec/factories/product_factory.rb
@@ -0,0 +1,20 @@
+FactoryGirl.define do
+
+	factory :a_product, class: Product do
+
+		name "pepperoni pizza"
+		categories "italian"
+		description "delicious"
+		image_urls "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQahg4PepBSDY4vJXOdGtS2Mfb5N-TnV83-Wy23kIoasjj5iywYww"
+		regular_price "15"
+		discount_price "10"
+		discount_start_time 12/01/14
+		discount_end_time 12/02/14
+		discount_inventory "25"
+		ordr "1"
+		delivery_method "pick-up"
+		restaurant_id "123"
+
+	end
+	
+end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,5 +2,65 @@ require 'rails_helper'
 require 'shoulda/matchers'
 
 describe Product do
-  
+
+	it "has a name" do
+		product = FactoryGirl.build_stubbed(:a_product)
+		expect(product.name).to_not be_nil
+	end
+
+	it "has a category" do
+		product = FactoryGirl.build_stubbed(:a_product)
+		expect(product.categories).to_not be_nil
+	end
+
+	it "has a description" do
+		product = FactoryGirl.build_stubbed(:a_product)
+		expect(product.description).to_not be_nil
+	end
+
+	it "has image_urls" do
+		product = FactoryGirl.build_stubbed(:a_product)
+		expect(product.image_urls).to_not be_nil
+	end
+
+	it "has regular_price" do
+		product = FactoryGirl.build_stubbed(:a_product)
+		expect(product.regular_price).to_not be_nil
+	end
+
+	it "has discount_price" do
+		product = FactoryGirl.build_stubbed(:a_product)
+		expect(product.discount_price).to_not be_nil
+	end
+
+	it "has discount_start_time" do
+		product = FactoryGirl.build_stubbed(:a_product)
+		expect(product.discount_start_time).to_not be_nil
+	end
+
+	it "has discount_end_time" do
+		product = FactoryGirl.build_stubbed(:a_product)
+		expect(product.discount_end_time).to_not be_nil
+	end
+
+	it "has discount_inventory" do
+		product = FactoryGirl.build_stubbed(:a_product)
+		expect(product.discount_inventory).to_not be_nil
+	end
+
+	it "has ordr" do
+		product = FactoryGirl.build_stubbed(:a_product)
+		expect(product.ordr).to_not be_nil
+	end
+
+	it "has delivery_method" do
+		product = FactoryGirl.build_stubbed(:a_product)
+		expect(product.delivery_method).to_not be_nil
+	end
+
+	it "has restaurant_id" do
+		product = FactoryGirl.build_stubbed(:a_product)
+		expect(product.restaurant_id).to_not be_nil
+	end
+    
 end
